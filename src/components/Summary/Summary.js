3fe7baa4fab9c30f5efcefe409efc98c8742cc0a
Eloi Pardo <epardo@bcn3dtechnologies.com>
2020-02-04 16:30:46 +0100

    get expenses by month (not finished)

--- src/components/Summary/Summary.js
+++ src/components/Summary/Summary.js
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { getExpensesByMonth } from "./../../services/expenses";
 
 const Summary = () => {
+    useEffect(() => {
+        const month = new Date().getMonth() + 1;
+        getExpensesByMonth(month).subscribe(expenses => {
+            console.log(expenses);
+        });
+    }, []);
     return <div></div>;
 };
 
