f6db26964a5846c3d518aafa5b5238b651b42016
Eloi Pardo <eloipr94@gmail.com>
2020-02-25 20:27:40 +0100

    nicer summary

--- src/components/Summary/Summary.js
+++ src/components/Summary/Summary.js
@@ -1,19 +1,36 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./Summary.scss";
 
 const Summary = ({ expenses }) => {
-    const getBalance = () => {
-        return expenses.reduce((acc, expense) => acc + (expense.isIncome ? -expense.amount : expense.amount), 0);
-    };
+    const balance = expenses.reduce((acc, expense) => acc + (expense.isIncome ? expense.amount : -expense.amount), 0);
 
-    const getTotalExpenses = () => {
-        return expenses.length;
-    };
+    const totalExpenses = expenses.reduce((acc, expense) => acc + (expense.isIncome ? 0 : expense.amount), 0);
+
+    const totalIncomes = expenses.reduce((acc, expense) => acc + (expense.isIncome ? expense.amount : 0), 0);
 
     return (
         <div>
-            <div>{getTotalExpenses()}</div>
-            <div>{getBalance()}</div>
+            <div className="summary-details">
+                <div className="summary-row">
+                    <span>Expenses: </span>
+                    <span>
+                        {new Intl.NumberFormat("es-ES", { style: "currency", currency: "EUR" }).format(totalExpenses)}
+                    </span>
+                </div>
+                <div className="summary-row">
+                    <span>Incomes: </span>
+                    <span>
+                        {new Intl.NumberFormat("es-ES", { style: "currency", currency: "EUR" }).format(totalIncomes)}
+                    </span>
+                </div>
+                <div className="summary-row">
+                    <span>Balance: </span>
+                    <span className={balance > 0 ? "positive-amount" : balance < 0 ? "negative-amount" : ""}>
+                        {new Intl.NumberFormat("es-ES", { style: "currency", currency: "EUR" }).format(balance)}
+                    </span>
+                </div>
+            </div>
         </div>
     );
 };
