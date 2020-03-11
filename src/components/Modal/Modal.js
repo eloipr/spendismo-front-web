de43a0a0fbfe11e2797f18f4a1516df9659f14c1
Eloi Pardo <eloipr94@gmail.com>
2020-03-11 17:34:22 +0100

    change how visibility is handled

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -4,15 +4,15 @@ import CloseIcon from "@material-ui/icons/Close";
 import "./Modal.scss";
 
 const Modal = ({ show, handleClose, children }) => {
-    const showHideClass = show ? "modal" : "modal hidden";
-
     return (
-        <div className={showHideClass}>
-            <section className="modal-content">
-                {children}
-                <CloseIcon className="button-close" onClick={handleClose}></CloseIcon>
-            </section>
-        </div>
+        show && (
+            <div className="modal">
+                <section className="modal-content">
+                    {children}
+                    <CloseIcon className="button-close" onClick={handleClose}></CloseIcon>
+                </section>
+            </div>
+        )
     );
 };
 
